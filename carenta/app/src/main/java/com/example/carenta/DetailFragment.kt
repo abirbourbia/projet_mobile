cc0f479b343402de970918561678f93897b6e2e3
Abir Bourbia <ja_bourbia@esi.dz>
2023-01-20 13:24:06 +0100

    just updated the mark

--- carenta/app/src/main/java/com/example/carenta/DetailFragment.kt
+++ carenta/app/src/main/java/com/example/carenta/DetailFragment.kt
@@ -37,6 +37,7 @@ class DetailFragment : Fragment() {
         if(car!=null) {
             binding.model.text = car.model
             binding.tarif.text = car.tarif+"0DA/H"
+            Glide.with(requireActivity()).load(url+car.marque).into(binding.mark)
             Glide.with(requireActivity()).load(
                 url+car.image).into(binding.image)
         }
